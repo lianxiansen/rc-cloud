a36f44e7b3b8f5e5d37d913f3a77c95329180070
liandy <13857652373@163.com>
2023-06-21 09:56:39 +0800

    新增服务mall

---
+++ rc-application/rc-app-mall-biz/src/test/java/com/example/rcappproductbiz/RcAppProductBizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rcappproductbiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcAppProductBizApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
