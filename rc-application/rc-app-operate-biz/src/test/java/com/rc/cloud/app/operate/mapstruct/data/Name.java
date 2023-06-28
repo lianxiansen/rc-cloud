71007f6e439c6a1ab465020d01dbf7ede0f8971f
liandy <13857652373@163.com>
2023-06-28 11:30:49 +0800

    test(operate): mapstruct测试
    新增mapstruct测试demo

---
+++ rc-application/rc-app-operate-biz/src/test/java/com/rc/cloud/app/operate/mapstruct/data/Name.java
@@ -0,0 +1,15 @@
+package com.rc.cloud.app.operate.mapstruct.data;
+
+import lombok.Data;
+
+/**
+ * @ClassName: Name
+ * @Author: liandy
+ * @Date: 2023/6/28 11:03
+ * @Description: TODO
+ */
+
+@Data
+public class Name{
+    private String value;
+}
