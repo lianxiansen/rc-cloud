2be01d8164d9d34100847181b4db65303efc510a
橄榄油 <575929054@qq.com>
2023-06-29 14:23:05 +0800

    完善角色管理

---
+++ rc-application/rc-app-system-biz/src/main/java/com/rc/cloud/app/system/vo/permission/role/RoleUserPageVO.java
@@ -0,0 +1,25 @@
+package com.rc.cloud.app.system.vo.permission.role;
+
+import com.rc.cloud.common.core.pojo.PageParam;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Schema(description = "分配角色查询")
+public class RoleUserPageVO extends PageParam {
+
+    @Schema(description = "角色ID")
+    private Long roleId;
+
+    @Schema(description = "用户名")
+    private String username;
+
+    @Schema(description = "手机号")
+    private String mobile;
+
+    @Schema(description = "性别")
+    private Integer sex;
+
+}
