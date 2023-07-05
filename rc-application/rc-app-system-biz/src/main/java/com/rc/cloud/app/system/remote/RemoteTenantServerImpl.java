55ad699b29d4f9934ab2136dda503f1b9ef06c76
橄榄油 <575929054@qq.com>
2023-07-05 16:24:50 +0800

    公共租户模块加入TenantFrameworkService配置

---
+++ rc-application/rc-app-system-biz/src/main/java/com/rc/cloud/app/system/remote/RemoteTenantServerImpl.java
@@ -0,0 +1,39 @@
+package com.rc.cloud.app.system.remote;
+
+import com.rc.cloud.app.system.service.tenant.TenantService;
+import com.rc.cloud.common.core.web.CodeResult;
+import com.rc.cloud.common.security.annotation.Inner;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * 供多租户远程调用
+ *
+ * @author oliveoil
+ * @date 2023-07-05
+ */
+@RestController
+@RequestMapping("/sys/tenant")
+public class RemoteTenantServerImpl {
+    @Resource
+    private TenantService tenantService;
+
+
+    @Inner
+    @GetMapping("/getTenantIdList")
+    public CodeResult<List<Long>> getTenantIdList() {
+        return CodeResult.ok(tenantService.getTenantIdList());
+    }
+
+
+    @Inner
+    @GetMapping("/validateTenant")
+    public CodeResult validateTenant(Long id) {
+        tenantService.validTenant(id);
+        return CodeResult.ok();
+    }
+}
