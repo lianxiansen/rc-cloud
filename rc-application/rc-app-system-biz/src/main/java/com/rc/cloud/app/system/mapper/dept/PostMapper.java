984a1479a8e6c206ac474eaf83918c398e7f4bfb
橄榄油 <575929054@qq.com>
2023-07-13 08:25:16 +0800

    岗位分页查询改为按任意字段排序

--- rc-application/rc-app-system-biz/src/main/java/com/rc/cloud/app/system/mapper/dept/PostMapper.java
+++ rc-application/rc-app-system-biz/src/main/java/com/rc/cloud/app/system/mapper/dept/PostMapper.java
@@ -1,11 +1,14 @@
 package com.rc.cloud.app.system.mapper.dept;
 
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.rc.cloud.app.system.model.dept.SysPostPO;
 import com.rc.cloud.app.system.vo.dept.post.PostExportReqVO;
 import com.rc.cloud.app.system.vo.dept.post.PostPageReqVO;
 import com.rc.cloud.common.core.pojo.PageResult;
 import com.rc.cloud.common.mybatis.core.mapper.BaseMapperX;
 import com.rc.cloud.common.mybatis.core.query.LambdaQueryWrapperX;
+import jodd.util.StringUtil;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.Collection;
@@ -22,11 +25,17 @@ public interface PostMapper extends BaseMapperX<SysPostPO> {
     }
 
     default PageResult<SysPostPO> selectPage(PostPageReqVO reqVO) {
-        return selectPage(reqVO, new LambdaQueryWrapperX<SysPostPO>()
-                .likeIfPresent(SysPostPO::getCode, reqVO.getCode())
-                .likeIfPresent(SysPostPO::getName, reqVO.getName())
-                .eqIfPresent(SysPostPO::getStatus, reqVO.getStatus())
-                .orderByDesc(SysPostPO::getId));
+        if (StringUtil.isEmpty(reqVO.getOrder())) {
+            reqVO.setOrder("id");
+            reqVO.setAsc(false);
+        }
+        QueryWrapper<SysPostPO> wrapper = new QueryWrapper<>();
+        wrapper.lambda()
+                .like(StringUtil.isNotEmpty(reqVO.getCode()), SysPostPO::getCode, reqVO.getCode())
+                .like(StringUtil.isNotEmpty(reqVO.getName()), SysPostPO::getName, reqVO.getName())
+                .eq(reqVO.getStatus() != null, SysPostPO::getStatus, reqVO.getStatus());
+        wrapper.orderBy(true, reqVO.getAsc(), StrUtil.toUnderlineCase(reqVO.getOrder()));
+        return selectPage(reqVO, wrapper);
     }
 
     default List<SysPostPO> selectList(PostExportReqVO reqVO) {
