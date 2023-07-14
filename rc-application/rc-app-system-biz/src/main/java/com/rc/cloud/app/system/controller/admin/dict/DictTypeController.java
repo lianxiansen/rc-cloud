5fcbc74b749fcdaf34af4e524638b77a7afb6ee6
橄榄油 <575929054@qq.com>
2023-07-14 09:32:52 +0800

    完成rc-app-system-biz下的controller包的代码规范与清理无用的代码

--- rc-application/rc-app-system-biz/src/main/java/com/rc/cloud/app/system/controller/admin/dict/DictTypeController.java
+++ rc-application/rc-app-system-biz/src/main/java/com/rc/cloud/app/system/controller/admin/dict/DictTypeController.java
@@ -1,7 +1,7 @@
 package com.rc.cloud.app.system.controller.admin.dict;
 
-import com.rc.cloud.app.system.model.dict.SysDictTypeDO;
 import com.rc.cloud.app.system.convert.dict.DictTypeConvert;
+import com.rc.cloud.app.system.model.dict.SysDictTypeDO;
 import com.rc.cloud.app.system.service.dict.DictTypeService;
 import com.rc.cloud.app.system.vo.dict.SysDictVO;
 import com.rc.cloud.app.system.vo.dict.type.*;
@@ -18,7 +18,11 @@ import javax.annotation.Resource;
 import javax.validation.Valid;
 import java.util.List;
 
-
+/**
+ * @author rc@hqf
+ * @date 2023/07/14
+ * @description 字典类型管理
+ */
 @Tag(name = "管理后台 - 字典类型")
 @RestController
 @RequestMapping("/sys/dict-type")
@@ -28,6 +32,12 @@ public class DictTypeController {
     @Resource
     private DictTypeService dictTypeService;
 
+    /**
+     * 创建字典类型
+     *
+     * @param reqVO 字典类型信息
+     * @return 字典类型编号
+     */
     @PostMapping("/create")
     @Operation(summary = "创建字典类型")
     @PreAuthorize("@pms.hasPermission('sys:dict:create')")
@@ -36,6 +46,12 @@ public class DictTypeController {
         return CodeResult.ok(dictTypeId);
     }
 
+    /**
+     * 更新字典类型
+     *
+     * @param reqVO 字典类型信息
+     * @return 是否成功
+     */
     @PutMapping("/update")
     @Operation(summary = "修改字典类型")
     @PreAuthorize("@pms.hasPermission('sys:dict:update')")
@@ -44,6 +60,12 @@ public class DictTypeController {
         return CodeResult.ok(true);
     }
 
+    /**
+     * 删除字典类型
+     *
+     * @param idList 字典类型编号列表
+     * @return 是否成功
+     */
     @DeleteMapping()
     @Operation(summary = "删除字典类型")
     @Parameter(name = "idList", description = "编号列表", required = true, example = "[1024,1025]")
@@ -53,6 +75,12 @@ public class DictTypeController {
         return CodeResult.ok(true);
     }
 
+    /**
+     * 获得字典类型的分页列表
+     *
+     * @param reqVO 查询条件
+     * @return 字典类型分页列表
+     */
     @Operation(summary = "/获得字典类型的分页列表")
     @GetMapping("/page")
     @PreAuthorize("@pms.hasPermission('sys:dict:query')")
@@ -60,6 +88,12 @@ public class DictTypeController {
         return CodeResult.ok(DictTypeConvert.INSTANCE.convertPage(dictTypeService.getDictTypePage(reqVO)));
     }
 
+    /**
+     * 获得字典类型详细
+     *
+     * @param id 字典类型编号
+     * @return 字典类型详细
+     */
     @Operation(summary = "/查询字典类型详细")
     @Parameter(name = "id", description = "编号", required = true, example = "1024")
     @GetMapping(value = "/{id}")
@@ -68,6 +102,11 @@ public class DictTypeController {
         return CodeResult.ok(DictTypeConvert.INSTANCE.convert(dictTypeService.getDictTypeById(id)));
     }
 
+    /**
+     * 获得全部字典类型列表
+     *
+     * @return 全部字典类型列表
+     */
     @GetMapping("/list-all-simple")
     @Operation(summary = "获得全部字典类型列表", description = "包括开启 + 禁用的字典类型，主要用于前端的下拉选项")
     // 无需添加权限认证，因为前端全局都需要
@@ -76,6 +115,11 @@ public class DictTypeController {
         return CodeResult.ok(DictTypeConvert.INSTANCE.convertList(list));
     }
 
+    /**
+     * 获得全部字典类型列表
+     *
+     * @return 全部字典类型列表
+     */
     @GetMapping("/dict/all")
     @Operation(summary = "全部字典数据")
     public CodeResult<List<SysDictVO>> all() {
