d9650cb0ab1562a8bade0ca58c7ae33518ee8b23
橄榄油 <575929054@qq.com>
2023-07-20 13:17:39 +0800

    system新增创建登录日志记录的远程调用接口

---
+++ rc-application/rc-app-system-biz/src/main/java/com/rc/cloud/app/system/controller/remote/RemoteLoginLogServerImpl.java
@@ -0,0 +1,43 @@
+
+package com.rc.cloud.app.system.controller.remote;
+
+import com.rc.cloud.app.system.api.logger.dto.LoginLogCreateReqDTO;
+import com.rc.cloud.app.system.api.user.dto.UserInfo;
+import com.rc.cloud.app.system.model.user.SysUserPO;
+import com.rc.cloud.app.system.service.logger.LoginLogService;
+import com.rc.cloud.common.core.web.CodeResult;
+import com.rc.cloud.common.security.annotation.Inner;
+import com.rc.cloud.common.tenant.core.context.TenantContextHolder;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+import static com.rc.cloud.app.system.enums.ErrorCodeConstants.USER_NOT_EXISTS;
+import static com.rc.cloud.common.core.exception.util.ServiceExceptionUtil.exception;
+
+/**
+ * @author oliveoil
+ * @date 2023-07-19 13:47
+ * @description 登录日志远程调用实现
+ */
+@RestController
+@RequestMapping("/sys/login-log")
+@Validated
+public class RemoteLoginLogServerImpl {
+
+    @Resource
+    private LoginLogService loginLogService;
+
+    /**
+     * 记录登录日志
+     *
+     * @param reqDTO 登录日志DTO
+     */
+    @Inner
+    @PostMapping("/create")
+    public void create(@Valid @RequestBody LoginLogCreateReqDTO reqDTO) {
+        loginLogService.createLoginLog(reqDTO);
+    }
+}
