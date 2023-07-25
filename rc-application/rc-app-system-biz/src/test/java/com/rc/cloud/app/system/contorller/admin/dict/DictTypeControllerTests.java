65e77cf4afe674c6bc0cac046ce18c466ecb1aa1
橄榄油 <575929054@qq.com>
2023-07-25 09:28:33 +0800

    完善创建数据字典类型的集成测试

--- rc-application/rc-app-system-biz/src/test/java/com/rc/cloud/app/system/contorller/admin/dict/DictTypeControllerTests.java
+++ rc-application/rc-app-system-biz/src/test/java/com/rc/cloud/app/system/contorller/admin/dict/DictTypeControllerTests.java
@@ -1,17 +1,18 @@
-/**
- * @author oliveoil
- * date 2023-06-13 09:46
- */
 package com.rc.cloud.app.system.contorller.admin.dict;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rc.cloud.app.system.controller.admin.v1.dict.DictTypeController;
+import com.rc.cloud.app.system.mapper.dict.DictTypeMapper;
+import com.rc.cloud.app.system.model.dict.SysDictDataPO;
+import com.rc.cloud.app.system.model.dict.SysDictTypePO;
 import com.rc.cloud.app.system.service.dict.DictTypeService;
 import com.rc.cloud.app.system.vo.dict.type.DictTypeCreateReqVO;
 import com.rc.cloud.app.system.vo.dict.type.DictTypeUpdateReqVO;
+import com.rc.cloud.common.core.enums.CommonStatusEnum;
 import com.rc.cloud.common.tenant.core.context.TenantContextHolder;
 import com.rc.cloud.common.test.annotation.RcTest;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -23,6 +24,8 @@ import org.springframework.web.context.WebApplicationContext;
 
 import javax.annotation.Resource;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -30,6 +33,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
+ * @author oliveoil
+ * @date 2023-06-13 09:46
  * 关联 {@link DictTypeController} 类
  */
 @RcTest
@@ -43,6 +48,9 @@ public class DictTypeControllerTests {
     @Resource
     private DictTypeService dictTypeService;
 
+    @Resource
+    private DictTypeMapper dictTypeMapper;
+
     @Qualifier("springSecurityFilterChain")
     @BeforeEach
     public void setup() {
@@ -51,6 +59,138 @@ public class DictTypeControllerTests {
                 .webAppContextSetup(context)
                 .apply(springSecurity())
                 .build();
+    }
+
+    /**
+     * @author rc@hqf
+     * @date 2023/07/24
+     * @description 创建字典类型相关测试
+     */
+    @Nested
+    class CreateDictTypeTests {
+
+        // happy path1: 创建成功
+        @Test
+        @WithMockUser(username = "admin", authorities = {"sys:dict:create"})
+        public void createDictType_success() throws Exception {
+            DictTypeCreateReqVO dictTypeCreateReqVO = new DictTypeCreateReqVO();
+            dictTypeCreateReqVO.setName("测试字段类型");
+            dictTypeCreateReqVO.setStatus(0);
+            dictTypeCreateReqVO.setRemark("备注");
+            dictTypeCreateReqVO.setType("test_type");
+            ObjectMapper mapper = new ObjectMapper();
+            String requestBody = mapper.writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(dictTypeCreateReqVO);
+            mvc.perform(post("/admin/dict-type/create")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(requestBody)
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.code").value(200))
+                    .andExpect(jsonPath("$.success").value(true))
+                    .andExpect(jsonPath("$.data").isNotEmpty());
+            SysDictTypePO dbDictTypePO = dictTypeMapper.selectByName(dictTypeCreateReqVO.getName());
+            assertNotEquals(null, dbDictTypePO);
+            assertEquals(dictTypeCreateReqVO.getName(), dbDictTypePO.getName());
+            assertEquals(dictTypeCreateReqVO.getStatus(), dbDictTypePO.getStatus());
+            assertEquals(dictTypeCreateReqVO.getRemark(), dbDictTypePO.getRemark());
+            assertEquals(dictTypeCreateReqVO.getType(), dbDictTypePO.getType());
+        }
+
+        // sad path1: 字典类型名称为空
+        @Test
+        @WithMockUser(username = "admin", authorities = {"sys:dict:create"})
+        public void createDictType_nameIsNull() throws Exception {
+            DictTypeCreateReqVO dictTypeCreateReqVO = new DictTypeCreateReqVO();
+            dictTypeCreateReqVO.setName(null);
+            dictTypeCreateReqVO.setStatus(0);
+            dictTypeCreateReqVO.setRemark("备注");
+            dictTypeCreateReqVO.setType("test_type");
+            ObjectMapper mapper = new ObjectMapper();
+            String requestBody = mapper.writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(dictTypeCreateReqVO);
+            mvc.perform(post("/admin/dict-type/create")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(requestBody)
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.code").value(10030))
+                    .andExpect(jsonPath("$.success").value(false))
+                    .andExpect(jsonPath("$.msg").value("请求参数不正确:字典名称不能为空"));
+        }
+
+        // sad path2: 字典类型type为空
+        @Test
+        @WithMockUser(username = "admin", authorities = {"sys:dict:create"})
+        public void createDictType_typeIsNull() throws Exception {
+            DictTypeCreateReqVO dictTypeCreateReqVO = new DictTypeCreateReqVO();
+            dictTypeCreateReqVO.setName("测试字段类型");
+            dictTypeCreateReqVO.setStatus(0);
+            dictTypeCreateReqVO.setRemark("备注");
+            dictTypeCreateReqVO.setType(null);
+            ObjectMapper mapper = new ObjectMapper();
+            String requestBody = mapper.writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(dictTypeCreateReqVO);
+            mvc.perform(post("/admin/dict-type/create")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(requestBody)
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.code").value(10030))
+                    .andExpect(jsonPath("$.success").value(false))
+                    .andExpect(jsonPath("$.msg").value("请求参数不正确:字典类型不能为空"));
+        }
+
+        // sad path3: 字典类型name已存在
+        @Test
+        @WithMockUser(username = "admin", authorities = {"sys:dict:create"})
+        public void createDictType_nameIsExist() throws Exception {
+            SysDictTypePO dictType1 = createDictType1();
+            DictTypeCreateReqVO dictTypeCreateReqVO = new DictTypeCreateReqVO();
+            dictTypeCreateReqVO.setName(dictType1.getName());
+            dictTypeCreateReqVO.setStatus(0);
+            dictTypeCreateReqVO.setRemark("备注");
+            dictTypeCreateReqVO.setType("test_type");
+            ObjectMapper mapper = new ObjectMapper();
+            String requestBody = mapper.writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(dictTypeCreateReqVO);
+            mvc.perform(post("/admin/dict-type/create")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(requestBody)
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.code").value(1002006003))
+                    .andExpect(jsonPath("$.success").value(false))
+                    .andExpect(jsonPath("$.msg").value("已经存在该名字的字典类型"));
+        }
+
+        // sad path4: 字典类型type已存在
+        @Test
+        @WithMockUser(username = "admin", authorities = {"sys:dict:create"})
+        public void createDictType_typeIsExist() throws Exception {
+            SysDictTypePO dictType1 = createDictType1();
+            DictTypeCreateReqVO dictTypeCreateReqVO = new DictTypeCreateReqVO();
+            dictTypeCreateReqVO.setName("测试字段类型");
+            dictTypeCreateReqVO.setStatus(0);
+            dictTypeCreateReqVO.setRemark("备注");
+            dictTypeCreateReqVO.setType(dictType1.getType());
+            ObjectMapper mapper = new ObjectMapper();
+            String requestBody = mapper.writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(dictTypeCreateReqVO);
+            mvc.perform(post("/admin/dict-type/create")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(requestBody)
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.code").value(1002006004))
+                    .andExpect(jsonPath("$.success").value(false))
+                    .andExpect(jsonPath("$.msg").value("已经存在该类型的字典类型"));
+        }
     }
 
     @Test
@@ -92,27 +232,6 @@ public class DictTypeControllerTests {
                 .andExpect(jsonPath("$.data.list[0].name").value("通用状态"));
     }
 
-    @Test
-    @WithMockUser(username = "admin", authorities = {"sys:dict:create"})
-    public void createDictType_success() throws Exception {
-        DictTypeCreateReqVO dictTypeCreateReqVO = new DictTypeCreateReqVO();
-        dictTypeCreateReqVO.setName("测试字段类型");
-        dictTypeCreateReqVO.setStatus(0);
-        dictTypeCreateReqVO.setRemark("备注");
-        dictTypeCreateReqVO.setType("test_type");
-        ObjectMapper mapper = new ObjectMapper();
-        String requestBody = mapper.writerWithDefaultPrettyPrinter()
-                .writeValueAsString(dictTypeCreateReqVO);
-        mvc.perform(post("/sys/dict-type/create")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(requestBody)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.code").value(200))
-                .andExpect(jsonPath("$.success").value(true))
-                .andExpect(jsonPath("$.data").isNotEmpty());
-    }
 
     @Test
     @WithMockUser(username = "admin", authorities = {"sys:dict:update"})
@@ -155,5 +274,25 @@ public class DictTypeControllerTests {
                 .andExpect(jsonPath("$.code").value(200))
                 .andExpect(jsonPath("$.success").value(true))
                 .andExpect(jsonPath("$.data").value(true));
+    }
+
+    private SysDictTypePO createDictType1() {
+        SysDictTypePO dictTypePO = new SysDictTypePO();
+        dictTypePO.setName("测试字段类型01");
+        dictTypePO.setStatus(CommonStatusEnum.ENABLE.getStatus());
+        dictTypePO.setRemark("备注11");
+        dictTypePO.setType("test_type1");
+        dictTypeMapper.insert(dictTypePO);
+        return dictTypePO;
+    }
+
+    private SysDictTypePO createDictType2() {
+        SysDictTypePO dictTypePO = new SysDictTypePO();
+        dictTypePO.setName("测试字段类型02");
+        dictTypePO.setStatus(CommonStatusEnum.ENABLE.getStatus());
+        dictTypePO.setRemark("备注22");
+        dictTypePO.setType("test_type2");
+        dictTypeMapper.insert(dictTypePO);
+        return dictTypePO;
     }
 }
