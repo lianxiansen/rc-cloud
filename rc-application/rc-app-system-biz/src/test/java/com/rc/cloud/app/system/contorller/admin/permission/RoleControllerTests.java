242c4dbe3b81637cfd7372a151345c7c0cfe5721
橄榄油 <575929054@qq.com>
2023-07-27 17:00:41 +0800

    完善角色的集成测试，增加几个sad path

--- rc-application/rc-app-system-biz/src/test/java/com/rc/cloud/app/system/contorller/admin/permission/RoleControllerTests.java
+++ rc-application/rc-app-system-biz/src/test/java/com/rc/cloud/app/system/contorller/admin/permission/RoleControllerTests.java
@@ -1,5 +1,6 @@
 package com.rc.cloud.app.system.contorller.admin.permission;
 
+import com.alibaba.fastjson.JSONObject;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rc.cloud.app.system.controller.admin.v1.permission.RoleController;
 import com.rc.cloud.app.system.mapper.permission.RoleMapper;
@@ -26,6 +27,8 @@ import javax.annotation.Resource;
 import java.util.HashSet;
 import java.util.Set;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -65,11 +68,47 @@ public class RoleControllerTests {
      */
     @Nested
     class CreateRoleTests {
+
+        // happy path: 创建角色成功
         @Test
         @WithMockUser(username = "admin", authorities = {"sys:role:create"})
         public void createRole_success() throws Exception {
             RoleCreateReqVO createReqVO = new RoleCreateReqVO();
             createReqVO.setName("测试角色");
+            createReqVO.setSort(3);
+            createReqVO.setCode("test_role");
+            createReqVO.setRemark("备注");
+            ObjectMapper mapper = new ObjectMapper();
+            String requestBody = mapper.writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(createReqVO);
+            String postRes = mvc.perform(post("/admin/role/create")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(requestBody)
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.code").value(200))
+                    .andExpect(jsonPath("$.success").value(true))
+                    .andExpect(jsonPath("$.data").isNotEmpty())
+                    .andReturn().getResponse().getContentAsString();   // 将相应的数据转换为字符串
+            String roleId = JSONObject.parseObject(postRes).get("data").toString();
+            SysRolePO role = roleMapper.selectById(roleId);
+            assertNotEquals(null, role);
+            assertEquals(createReqVO.getName(), role.getName());
+            assertEquals(createReqVO.getSort(), role.getSort());
+            assertEquals(createReqVO.getCode(), role.getCode());
+            assertEquals(createReqVO.getRemark(), role.getRemark());
+            assertEquals(CommonStatusEnum.ENABLE.getStatus(), role.getStatus());
+            assertEquals(2, role.getType()); // 默认为2
+        }
+
+        // sad path1: 角色名称已存在
+        @Test
+        @WithMockUser(username = "admin", authorities = {"sys:role:create"})
+        public void createRole_failed_nameExist() throws Exception {
+            SysRolePO role = createRole();
+            RoleCreateReqVO createReqVO = new RoleCreateReqVO();
+            createReqVO.setName(role.getName());
             createReqVO.setSort(3);
             createReqVO.setCode("test_role");
             createReqVO.setRemark("备注");
@@ -82,9 +121,33 @@ public class RoleControllerTests {
                             .accept(MediaType.APPLICATION_JSON))
                     .andDo(print())
                     .andExpect(status().isOk())
-                    .andExpect(jsonPath("$.code").value(200))
-                    .andExpect(jsonPath("$.success").value(true))
-                    .andExpect(jsonPath("$.data").isNotEmpty());
+                    .andExpect(jsonPath("$.code").value(1002002001))
+                    .andExpect(jsonPath("$.success").value(false))
+                    .andExpect(jsonPath("$.msg").value("已经存在名为【" + createReqVO.getName() + "】的角色"));
+        }
+
+        // sad path2: 角色编码已存在
+        @Test
+        @WithMockUser(username = "admin", authorities = {"sys:role:create"})
+        public void createRole_failed_codeExist() throws Exception {
+            SysRolePO role = createRole();
+            RoleCreateReqVO createReqVO = new RoleCreateReqVO();
+            createReqVO.setName("测试角色111");
+            createReqVO.setSort(3);
+            createReqVO.setCode(role.getCode());
+            createReqVO.setRemark("备注");
+            ObjectMapper mapper = new ObjectMapper();
+            String requestBody = mapper.writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(createReqVO);
+            mvc.perform(post("/admin/role/create")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(requestBody)
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.code").value(1002002002))
+                    .andExpect(jsonPath("$.success").value(false))
+                    .andExpect(jsonPath("$.msg").value("已经存在编码为【" + createReqVO.getCode() + "】的角色"));
         }
     }
 
@@ -95,13 +158,15 @@ public class RoleControllerTests {
      */
     @Nested
     class UpdateRoleTests {
+
+        // happy path: 更新角色成功
         @Test
         @WithMockUser(username = "admin", authorities = {"sys:role:update"})
         public void updateRole_success() throws Exception {
             SysRolePO role = createRole();
             RoleUpdateReqVO updateReqVO = new RoleUpdateReqVO();
             updateReqVO.setId(role.getId());
-            updateReqVO.setName("测试角色");
+            updateReqVO.setName("测试角色111");
             updateReqVO.setSort(77);
             updateReqVO.setCode("test_role");
             updateReqVO.setRemark("备注");
@@ -117,9 +182,89 @@ public class RoleControllerTests {
                     .andExpect(jsonPath("$.code").value(200))
                     .andExpect(jsonPath("$.success").value(true))
                     .andExpect(jsonPath("$.data").value(true));
+            SysRolePO dbRole = roleMapper.selectById(role.getId());
+            assertEquals(updateReqVO.getName(), dbRole.getName());
+            assertEquals(updateReqVO.getSort(), dbRole.getSort());
+            assertEquals(updateReqVO.getCode(), dbRole.getCode());
+            assertEquals(updateReqVO.getRemark(), dbRole.getRemark());
+        }
+
+        // sad path1: 角色名称已存在
+        @Test
+        @WithMockUser(username = "admin", authorities = {"sys:role:update"})
+        public void updateRole_failed_nameExist() throws Exception {
+            SysRolePO role = createRole();
+            SysRolePO role2 = createRole2();
+            RoleUpdateReqVO updateReqVO = new RoleUpdateReqVO();
+            updateReqVO.setId(role.getId());
+            updateReqVO.setName(role2.getName());
+            updateReqVO.setSort(77);
+            updateReqVO.setCode("test_role");
+            updateReqVO.setRemark("备注");
+            ObjectMapper mapper = new ObjectMapper();
+            String requestBody = mapper.writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(updateReqVO);
+            mvc.perform(put("/admin/role/update")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(requestBody)
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.code").value(1002002001))
+                    .andExpect(jsonPath("$.success").value(false))
+                    .andExpect(jsonPath("$.msg").value("已经存在名为【" + updateReqVO.getName() + "】的角色"));
+        }
+
+        // sad path2: 角色编码已存在
+        @Test
+        @WithMockUser(username = "admin", authorities = {"sys:role:update"})
+        public void updateRole_failed_codeExist() throws Exception {
+            SysRolePO role = createRole();
+            SysRolePO role2 = createRole2();
+            RoleUpdateReqVO updateReqVO = new RoleUpdateReqVO();
+            updateReqVO.setId(role.getId());
+            updateReqVO.setName("测试角色111");
+            updateReqVO.setSort(77);
+            updateReqVO.setCode(role2.getCode());
+            updateReqVO.setRemark("备注");
+            ObjectMapper mapper = new ObjectMapper();
+            String requestBody = mapper.writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(updateReqVO);
+            mvc.perform(put("/admin/role/update")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(requestBody)
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.code").value(1002002002))
+                    .andExpect(jsonPath("$.success").value(false))
+                    .andExpect(jsonPath("$.msg").value("已经存在编码为【" + updateReqVO.getCode() + "】的角色"));
         }
 
         // sad path: 不能操作内置角色
+        @Test
+        @WithMockUser(username = "admin", authorities = {"sys:role:update"})
+        public void updateRole_failed_cannotOperateBuiltInRole() throws Exception {
+            SysRolePO role = createInnerRole();
+            RoleUpdateReqVO updateReqVO = new RoleUpdateReqVO();
+            updateReqVO.setId(role.getId());
+            updateReqVO.setName("测试角色111");
+            updateReqVO.setSort(77);
+            updateReqVO.setCode("test_role");
+            updateReqVO.setRemark("备注");
+            ObjectMapper mapper = new ObjectMapper();
+            String requestBody = mapper.writerWithDefaultPrettyPrinter()
+                    .writeValueAsString(updateReqVO);
+            mvc.perform(put("/admin/role/update")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(requestBody)
+                            .accept(MediaType.APPLICATION_JSON))
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.code").value(1002002003))
+                    .andExpect(jsonPath("$.success").value(false))
+                    .andExpect(jsonPath("$.msg").value("不能操作类型为系统内置的角色"));
+        }
     }
 
     /**
@@ -148,6 +293,8 @@ public class RoleControllerTests {
                     .andExpect(jsonPath("$.code").value(200))
                     .andExpect(jsonPath("$.success").value(true))
                     .andExpect(jsonPath("$.data").value(true));
+            SysRolePO dbRole = roleMapper.selectById(role.getId());
+            assertEquals(updateStatusReqVO.getStatus(), dbRole.getStatus());
         }
     }
 
@@ -175,6 +322,8 @@ public class RoleControllerTests {
                     .andExpect(jsonPath("$.code").value(200))
                     .andExpect(jsonPath("$.success").value(true))
                     .andExpect(jsonPath("$.data").value(true));
+            SysRolePO dbRolePO = roleMapper.selectById(role.getId());
+            assertEquals(null, dbRolePO);
         }
     }
 
@@ -193,7 +342,12 @@ public class RoleControllerTests {
                     .andDo(print())
                     .andExpect(status().isOk())
                     .andExpect(jsonPath("$.code").value(200))
-                    .andExpect(jsonPath("$.data.name").value(role.getName()));
+                    .andExpect(jsonPath("$.data.name").value(role.getName()))
+                    .andExpect(jsonPath("$.data.sort").value(role.getSort()))
+                    .andExpect(jsonPath("$.data.code").value(role.getCode()))
+                    .andExpect(jsonPath("$.data.remark").value(role.getRemark()))
+                    .andExpect(jsonPath("$.data.status").value(role.getStatus()))
+                    .andExpect(jsonPath("$.data.type").value(role.getType()));
         }
     }
 
@@ -216,7 +370,12 @@ public class RoleControllerTests {
                     .andExpect(jsonPath("$.data.total").value(1))
                     .andExpect(jsonPath("$.data.list").isArray())
                     .andExpect(jsonPath("$.data.list").isNotEmpty())
-                    .andExpect(jsonPath("$.data.list[0].name").value(role.getName()));
+                    .andExpect(jsonPath("$.data.list[0].name").value(role.getName()))
+                    .andExpect(jsonPath("$.data.list[0].sort").value(role.getSort()))
+                    .andExpect(jsonPath("$.data.list[0].code").value(role.getCode()))
+                    .andExpect(jsonPath("$.data.list[0].remark").value(role.getRemark()))
+                    .andExpect(jsonPath("$.data.list[0].status").value(role.getStatus()))
+                    .andExpect(jsonPath("$.data.list[0].type").value(role.getType()));
         }
     }
 
@@ -238,6 +397,7 @@ public class RoleControllerTests {
                     .andExpect(jsonPath("$.success").value(true))
                     .andExpect(jsonPath("$.data").isArray())
                     .andExpect(jsonPath("$.data").isNotEmpty())
+                    .andExpect(jsonPath("$.data[0].id").value(role.getId()))
                     .andExpect(jsonPath("$.data[0].name").value(role.getName()));
         }
     }
@@ -250,6 +410,30 @@ public class RoleControllerTests {
         role.setRemark("备注");
         role.setStatus(CommonStatusEnum.ENABLE.getStatus());
         role.setType(2);
+        roleMapper.insert(role);
+        return role;
+    }
+
+    private SysRolePO createRole2() {
+        SysRolePO role = new SysRolePO();
+        role.setName("测试角色22");
+        role.setSort(33);
+        role.setCode("test_role2");
+        role.setRemark("备注2");
+        role.setStatus(CommonStatusEnum.ENABLE.getStatus());
+        role.setType(2);
+        roleMapper.insert(role);
+        return role;
+    }
+
+    private SysRolePO createInnerRole() {
+        SysRolePO role = new SysRolePO();
+        role.setName("测试角色0");
+        role.setSort(0);
+        role.setCode("test_role0");
+        role.setRemark("备注0");
+        role.setStatus(CommonStatusEnum.ENABLE.getStatus());
+        role.setType(1);
         roleMapper.insert(role);
         return role;
     }
