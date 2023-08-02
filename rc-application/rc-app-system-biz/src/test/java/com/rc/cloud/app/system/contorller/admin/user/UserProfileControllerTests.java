d10d0572d5dae09d8291b6edb83ff3f36f1bc6e3
橄榄油 <575929054@qq.com>
2023-08-02 15:15:11 +0800

    加回用户信息相关的接口和测试

---
+++ rc-application/rc-app-system-biz/src/test/java/com/rc/cloud/app/system/contorller/admin/user/UserProfileControllerTests.java
@@ -0,0 +1,104 @@
+/**
+ * @author oliveoil
+ * date 2023-06-13 13:23
+ */
+package com.rc.cloud.app.system.contorller.admin.user;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rc.cloud.app.system.controller.admin.v1.user.UserProfileController;
+import com.rc.cloud.app.system.vo.user.profile.UserProfileUpdatePasswordReqVO;
+import com.rc.cloud.app.system.vo.user.profile.UserProfileUpdateReqVO;
+import com.rc.cloud.common.core.enums.SexEnum;
+import com.rc.cloud.common.tenant.core.context.TenantContextHolder;
+import com.rc.cloud.common.test.annotation.RcTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * 关联 {@link UserProfileController} 类
+ */
+@RcTest
+public class UserProfileControllerTests {
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Qualifier("springSecurityFilterChain")
+    @BeforeEach
+    public void setup() {
+        TenantContextHolder.setTenantId("1");
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @Test
+    @WithMockUser("admin")
+    public void getUserProfileById_success() throws Exception {
+        mvc.perform(get("/sys/user/profile/get"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(200))
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data.username").value("admin"));
+    }
+
+    @Test
+    @WithMockUser("admin")
+    public void updateUserProfile_success() throws Exception {
+        UserProfileUpdateReqVO updateReqVO = new UserProfileUpdateReqVO();
+        updateReqVO.setNickname("admin1");
+        updateReqVO.setEmail("32737238@qq.com");
+        updateReqVO.setMobile("13666666666");
+        updateReqVO.setSex(SexEnum.FEMALE.getSex());
+        ObjectMapper mapper = new ObjectMapper();
+        String requestBody = mapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(updateReqVO);
+        mvc.perform(put("/sys/user/profile/update")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(200))
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data").value(true));
+    }
+
+    @Test
+    @WithMockUser("admin")
+    public void updatePassword_success() throws Exception {
+        UserProfileUpdatePasswordReqVO updateReqVO = new UserProfileUpdatePasswordReqVO();
+        updateReqVO.setOldPassword("123456");
+        updateReqVO.setNewPassword("1234567");
+        ObjectMapper mapper = new ObjectMapper();
+        String requestBody = mapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(updateReqVO);
+        mvc.perform(put("/sys/user/profile/update-password")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.code").value(200))
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.data").isNotEmpty());
+    }
+}
