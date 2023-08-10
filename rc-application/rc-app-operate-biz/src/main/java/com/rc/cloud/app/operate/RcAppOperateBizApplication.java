e54fd48f2fec2b5cc575fbeff5f8561ba36b9bc3
liandy <13857652373@163.com>
2023-08-10 11:17:52 +0800

    feature(operate): 启用资源服务
    接口资源权限认证依赖common-security组件

--- rc-application/rc-app-operate-biz/src/main/java/com/rc/cloud/app/operate/RcAppOperateBizApplication.java
+++ rc-application/rc-app-operate-biz/src/main/java/com/rc/cloud/app/operate/RcAppOperateBizApplication.java
@@ -1,6 +1,7 @@
 package com.rc.cloud.app.operate;
 
 import com.rc.cloud.common.feign.EnableRcFeignClients;
+import com.rc.cloud.common.security.annotation.EnableRcResourceServer;
 import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @EnableRcFeignClients
 @DubboComponentScan(basePackages = "com.rc.cloud.app.operate")
+@EnableRcResourceServer
 public class RcAppOperateBizApplication {
 
     public static void main(String[] args) {
