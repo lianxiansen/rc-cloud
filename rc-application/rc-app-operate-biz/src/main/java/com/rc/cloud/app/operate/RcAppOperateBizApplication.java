76ddd94352a79ca69b3dfce2eb62b1eb42eaa537
liandy <13857652373@163.com>
2023-08-10 10:29:16 +0800

    fix(operate):
    1.引入dubbo
    2.优化yaml文件

--- rc-application/rc-app-operate-biz/src/main/java/com/rc/cloud/app/operate/RcAppOperateBizApplication.java
+++ rc-application/rc-app-operate-biz/src/main/java/com/rc/cloud/app/operate/RcAppOperateBizApplication.java
@@ -1,15 +1,15 @@
 package com.rc.cloud.app.operate;
 
 import com.rc.cloud.common.feign.EnableRcFeignClients;
+import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableRcFeignClients
+@DubboComponentScan(basePackages = "com.rc.cloud.app.operate")
 public class RcAppOperateBizApplication {
 
     public static void main(String[] args) {
